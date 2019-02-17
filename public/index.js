8d8a0259487b13d7780eea9bb1a7e2c07272ba09
p.rodriguez673 <p.rodriguez673@mybvc.ca>
2019-02-16 19:32:00 -0700

    Added reading data from arduino with socket io

--- public/index.js
+++ public/index.js
@@ -1,10 +1,18 @@
-function getParkings(){
-    fetch('test-data.json')
-    .then((res) => res.json())
-    .then((data) => {
-      let output = '<h2>Parkings</h2>';
-      console.log(data);
-    });
-  }
+// function getParkings(){
+//     fetch('test-data.json')
+//     .then((res) => res.json())
+//     .then((data) => {
+//       let output = '<h2>Parkings</h2>';
+//       console.log(data);
+//     });
+//   }
 
-  getParkings();
+//   getParkings();
+
+const socket = io();
+
+io.connect('http://localhost:8080');
+
+socket.on('status', function(data) {
+  console.log(data.temp);
+});
