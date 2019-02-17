905e74011d465c1e779d2e4261ead6b7b927d605
Karamullah Agha <k.agha809@mybvc.ca>
2019-02-17 02:56:31 -0700

    updating dom after selecting new parking spot

--- public/index.js
+++ public/index.js
@@ -3,3 +3,20 @@ const socket = io();
 socket.on('data', data => {
   console.log(data);
 });
+
+
+var parkingListContainer = document.getElementById("parkingList");
+var parkingLists = parkingListContainer.getElementsByClassName("list-group-item list-group-item-action");
+
+
+for (var i = 0; i < parkingLists.length; i++) {
+  parkingLists[i].addEventListener("click", updateDOM);
+}
+
+
+function updateDOM() {
+  var selected = document.getElementsByClassName("list-group-item list-group-item-action active");
+  selected[0].className = selected[0].className.replace("active", " ");
+  this.className += "list-group-item list-group-item-action active";
+  document.getElementById("title").innerHTML = this.innerHTML;
+}
